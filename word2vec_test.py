7680f55e0fe1e3aa8f447a9b229154e39b72e0ba
Gordon Ding <gdding@hotmail.com>
2020-10-11 11:51:48 +0800

    my modified word2vec train and test coding

---
+++ word2vec_test.py
@@ -0,0 +1,129 @@
+import numpy as np
+import pandas as pd
+import argparse
+
+# load word2vec model from the file
+def load_model(fi):
+    fi = open(fi, 'r')
+
+    # vocab size and dim
+    line = fi.readline().strip().split()
+    vocab_size, dim = int(line[0]), int(line[1])
+    print 'vocab_size=%d, dim=%d' % (vocab_size, dim)
+
+    vocab_vecs = [] # each item in the list is the word vector
+    vocab_hash = {} # each item in the dict is word:index
+    n = 0
+    for line in fi:
+        tokens = line.strip().split()
+        word = tokens[0]
+        vec = [float(v) for v in tokens[1:]]
+        vec = vec / np.linalg.norm(vec) # vector normalization
+        vocab_hash[word] = n
+        vocab_vecs.append(vec)
+        n += 1
+
+    assert n == vocab_size, 'load model error: unique word count loaded is not equal to vocab_size!'
+    fi.close()
+
+    return vocab_vecs, vocab_hash
+
+
+# find the best analogy word according the model
+# w* = w1 - w2 + w3
+def find_analogy(w1, w2, w3, vocab_vecs, vocab_hash):
+    dim = len(vocab_vecs[0])
+    w1 = w1.lower()
+    w2 = w2.lower()
+    w3 = w3.lower()
+    v1 = np.zeros(dim)
+    v2 = np.zeros(dim)
+    v3 = np.zeros(dim)
+    if w1 in vocab_hash: v1 = np.array(vocab_vecs[vocab_hash[w1]], dtype=np.float)
+    if w2 in vocab_hash: v2 = np.array(vocab_vecs[vocab_hash[w2]], dtype=np.float)
+    if w3 in vocab_hash: v3 = np.array(vocab_vecs[vocab_hash[w3]], dtype=np.float)
+    v4 = v3 - v1 + v2
+    nv4 = v4 / np.linalg.norm(v4)
+
+    # find the most similar vector and the word
+    max_sim = 0.0
+    max_word = 'unknown'
+    for w in vocab_hash:
+        if w == w1 or w == w2 or w == w3: continue
+        v = np.array(vocab_vecs[vocab_hash[w]], dtype=np.float)
+        sim = np.dot(v, nv4) # Cosine similarity
+        #euc = np.linalg.norm(v - nv4) # Euclidean distance
+        if sim > max_sim:
+            max_sim = sim
+            max_word = w
+
+    return max_word, max_sim
+
+
+# compute spearman correlation coefficent according wordsim353 test file
+def test_wordsim353(fi, vocab_vecs, vocab_hash):
+    fi = open(fi, 'r')
+    assert fi, 'open file error!'
+
+    vec1 = []
+    vec2 = []
+    for line in fi:
+        tokens = line.strip().split()
+        word1 = tokens[0].lower()
+        word2 = tokens[1].lower()
+        vec1.append(float(tokens[2]))
+
+        sim = 0.0
+        if word1 in vocab_hash and word2 in vocab_hash:
+            v1 = vocab_vecs[vocab_hash[word1]]
+            v2 = vocab_vecs[vocab_hash[word2]]
+            sim = np.dot(v1, v2) # consine similarity
+        else:
+            print 'Warning: %s or %s not found in vocab.' % (word1, word2)
+        print word1, '\t', word2, '\t', tokens[2], '\t', sim
+
+        vec2.append(sim)
+
+    X1 = pd.Series(vec1)
+    Y1 = pd.Series(vec2)
+    r = X1.corr(Y1, method='spearman')
+    print 'spearman correlation for wordsim353 is %.4f' % r
+
+    fi.close()
+
+
+# compute accuracy according to analogy test file
+def test_analogy(fi, vocab_vecs, vocab_hash):
+    fi = open(fi, 'r')
+    assert fi, 'open file error!'
+
+    acc_num = 0
+    total_num = 0
+    for line in fi:
+        tokens = line.strip().split()
+        if len(tokens) != 4: continue
+        w, s = find_analogy(tokens[0], tokens[1], tokens[2], vocab_vecs, vocab_hash)
+        if w == tokens[3].lower(): acc_num += 1
+        total_num += 1
+        print '[ %d/%d ] %s %s %s \t\t--> %s' % (acc_num, total_num, tokens[0], tokens[1], tokens[2], w)
+
+    print 'Accuracy for Google analogy is %d/%d (%.4f)' % (acc_num, total_num, float(acc_num) / total_num)
+    fi.close()
+
+
+def test_model(model, et, test):
+    vocab_vecs, vocab_hash = load_model(model)
+    if et == 0:
+        test_wordsim353(test, vocab_vecs, vocab_hash)
+    else:
+        test_analogy(test, vocab_vecs, vocab_hash)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-model', help='model file', dest='model', required=True)
+    parser.add_argument('-et', help='evaluation type: 0 for wordsim353, 1 for Google Analogy', dest='et', required=True, type=int)
+    parser.add_argument('-test', help='test file to be evaluated', dest='test', required=True)
+    args = parser.parse_args()
+
+    test_model(args.model, args.et, args.test)
--- word2vec_train.py
+++ word2vec_train.py
@@ -4,23 +4,23 @@ import struct
 import sys
 import time
 import warnings
-
 import numpy as np
-
 from multiprocessing import Pool, Value, Array
+
 
 class VocabItem:
     def __init__(self, word):
         self.word = word
         self.count = 0
-        self.path = None # Path (list of indices) from the root to the word (leaf)
+        self.path = None # Path(list of indices) from the root to the word(leaf)
         self.code = None # Huffman encoding
+
 
 class Vocab:
     def __init__(self, fi, min_count):
-        vocab_items = []
-        vocab_hash = {}
-        word_count = 0
+        vocab_items = [] # each item in the list is an unique word
+        vocab_hash = {} # each item in the dict is word:index
+        word_count = 0 # total word count in the train file
         fi = open(fi, 'r')
 
         # Add special tokens <bol> (beginning of line) and <eol> (end of line)
@@ -34,29 +34,34 @@ class Vocab:
                 if token not in vocab_hash:
                     vocab_hash[token] = len(vocab_items)
                     vocab_items.append(VocabItem(token))
-                    
-                #assert vocab_items[vocab_hash[token]].word == token, 'Wrong vocab_hash index'
+
+                # assert vocab_items[vocab_hash[token]].word == token, "Wrong vocab_hash index"
                 vocab_items[vocab_hash[token]].count += 1
                 word_count += 1
-            
+
                 if word_count % 10000 == 0:
-                    sys.stdout.write("\rReading word %d" % word_count)
+                    sys.stdout.write('\rReading word %d' % word_count)
                     sys.stdout.flush()
 
-            # Add special tokens <bol> (beginning of line) and <eol> (end of line)
+            # Add special tokens <bol> and <eol>
             vocab_items[vocab_hash['<bol>']].count += 1
             vocab_items[vocab_hash['<eol>']].count += 1
             word_count += 2
 
-        self.bytes = fi.tell()
-        self.vocab_items = vocab_items         # List of VocabItem objects
-        self.vocab_hash = vocab_hash           # Mapping from each token to its index in vocab
-        self.word_count = word_count           # Total number of words in train file
+            # max word count limitation -- for test
+            #if word_count > 10000000: break;
 
-        # Add special token <unk> (unknown),
-        # merge words occurring less than min_count into <unk>, and
-        # sort vocab in descending order by frequency in train file
+        self.bytes = fi.tell()
+        self.vocab_items = vocab_items # List of VocabItem objects
+        self.vocab_hash = vocab_hash   # Mapping from each token to its index in vocab
+        self.word_count = word_count   # Total number of words in train file
+
+        # Add special token <unk> (unknown)
+        print
+        print 'Merge words occurring less than min_count into <unk>, and'
+        print 'sort vocab in desending order by frequency in train file...'
         self.__sort(min_count)
+        fi.close()
 
         #assert self.word_count == sum([t.count for t in self.vocab_items]), 'word_count and sum of t.count do not agree'
         print 'Total words in training file: %d' % self.word_count
@@ -79,11 +84,11 @@ class Vocab:
         tmp = []
         tmp.append(VocabItem('<unk>'))
         unk_hash = 0
-        
-        count_unk = 0
+
+        unk_count = 0
         for token in self.vocab_items:
             if token.count < min_count:
-                count_unk += 1
+                unk_count += 1
                 tmp[unk_hash].count += token.count
             else:
                 tmp.append(token)
@@ -93,13 +98,13 @@ class Vocab:
         # Update vocab_hash
         vocab_hash = {}
         for i, token in enumerate(tmp):
-            vocab_hash[token.word] = i
+            vocab_hash[token.word] = i;
 
         self.vocab_items = tmp
         self.vocab_hash = vocab_hash
-
         print
-        print 'Unknown vocab size:', count_unk
+        print 'Unknown vocab size:', unk_count
+        return
 
     def indices(self, tokens):
         return [self.vocab_hash[token] if token in self else self.vocab_hash['<unk>'] for token in tokens]
@@ -110,7 +115,7 @@ class Vocab:
         count = [t.count for t in self] + [1e15] * (vocab_size - 1)
         parent = [0] * (2 * vocab_size - 2)
         binary = [0] * (2 * vocab_size - 2)
-        
+
         pos1 = vocab_size - 1
         pos2 = vocab_size
 
@@ -149,7 +154,6 @@ class Vocab:
         for i, token in enumerate(self):
             path = [] # List of indices from the leaf to the root
             code = [] # Binary Huffman encoding from the leaf to the root
-
             node_idx = i
             while node_idx < root_idx:
                 if node_idx >= vocab_size: path.append(node_idx)
@@ -161,6 +165,7 @@ class Vocab:
             token.path = [j - vocab_size for j in path[::-1]]
             token.code = code[::-1]
 
+
 class UnigramTable:
     """
     A list of indices of tokens in the vocab following a power law distribution,
@@ -171,22 +176,25 @@ class UnigramTable:
         power = 0.75
         norm = sum([math.pow(t.count, power) for t in vocab]) # Normalizing constant
 
-        table_size = 1e8 # Length of the unigram table
+        table_size = 10000000 # Length of the unigram table
         table = np.zeros(table_size, dtype=np.uint32)
 
         print 'Filling unigram table'
         p = 0 # Cumulative probability
         i = 0
         for j, unigram in enumerate(vocab):
-            p += float(math.pow(unigram.count, power))/norm
+            p += float(math.pow(unigram.count, power)) / norm
+            #print 'j=%d, word=%s, count=%d, p=%.6f' % (j, unigram.word, unigram.count, p)
             while i < table_size and float(i) / table_size < p:
                 table[i] = j
                 i += 1
+
         self.table = table
 
     def sample(self, count):
         indices = np.random.randint(low=0, high=len(self.table), size=count)
         return [self.table[i] for i in indices]
+
 
 def sigmoid(z):
     if z > 6:
@@ -196,8 +204,9 @@ def sigmoid(z):
     else:
         return 1 / (1 + math.exp(-z))
 
+
 def init_net(dim, vocab_size):
-    # Init syn0 with random numbers from a uniform distribution on the interval [-0.5, 0.5]/dim
+    # Init syn0 with random numbers from a uniform distribution on the interval [-0.5,0.5]/dim
     tmp = np.random.uniform(low=-0.5/dim, high=0.5/dim, size=(vocab_size, dim))
     syn0 = np.ctypeslib.as_ctypes(tmp)
     syn0 = Array(syn0._type_, syn0, lock=False)
@@ -205,31 +214,28 @@ def init_net(dim, vocab_size):
     # Init syn1 with zeros
     tmp = np.zeros(shape=(vocab_size, dim))
     syn1 = np.ctypeslib.as_ctypes(tmp)
-    syn1 = Array(syn1._type_, syn1, lock=False)
+    syn1 = Array(syn0._type_, syn1, lock=False)
 
-    return (syn0, syn1)
+    return syn0, syn1
+
 
 def train_process(pid):
     # Set fi to point to the right chunk of training file
     start = vocab.bytes / num_processes * pid
     end = vocab.bytes if pid == num_processes - 1 else vocab.bytes / num_processes * (pid + 1)
     fi.seek(start)
-    #print 'Worker %d beginning training at %d, ending at %d' % (pid, start, end)
+    print 'Worker %d beginning training at %d, ending at %d' % (pid, start, end)
 
     alpha = starting_alpha
-
     word_count = 0
     last_word_count = 0
 
     while fi.tell() < end:
         line = fi.readline().strip()
-        # Skip blank lines
-        if not line:
-            continue
+        if not line: continue # skip blank lines
 
         # Init sent, a list of indices of words in line
         sent = vocab.indices(['<bol>'] + line.split() + ['<eol>'])
-
         for sent_pos, token in enumerate(sent):
             if word_count % 10000 == 0:
                 global_word_count.value += (word_count - last_word_count)
@@ -237,7 +243,7 @@ def train_process(pid):
 
                 # Recalculate alpha
                 alpha = starting_alpha * (1 - float(global_word_count.value) / vocab.word_count)
-                if alpha < starting_alpha * 0.0001: alpha = starting_alpha * 0.0001
+                if alpha < starting_alpha *0.0001: alpha = starting_alpha * 0.0001
 
                 # Print progress info
                 sys.stdout.write("\rAlpha: %f Progress: %d of %d (%.2f%%)" %
@@ -245,11 +251,11 @@ def train_process(pid):
                                   float(global_word_count.value) / vocab.word_count * 100))
                 sys.stdout.flush()
 
-            # Randomize window size, where win is the max window size
+            # get context according to window size
             current_win = np.random.randint(low=1, high=win+1)
             context_start = max(sent_pos - current_win, 0)
             context_end = min(sent_pos + current_win + 1, len(sent))
-            context = sent[context_start:sent_pos] + sent[sent_pos+1:context_end] # Turn into an iterator?
+            context = sent[context_start:sent_pos] + sent[sent_pos+1:context_end]
 
             # CBOW
             if cbow:
@@ -267,16 +273,15 @@ def train_process(pid):
                     classifiers = zip(vocab[token].path, vocab[token].code)
                 for target, label in classifiers:
                     z = np.dot(neu1, syn1[target])
-                    p = sigmoid(z)
-                    g = alpha * (label - p)
+                    g = alpha * (label - sigmoid(z))
                     neu1e += g * syn1[target] # Error to backpropagate to syn0
-                    syn1[target] += g * neu1  # Update syn1
+                    syn1[target] += g * neu1 # Update syn1
 
                 # Update syn0
                 for context_word in context:
                     syn0[context_word] += neu1e
 
-            # Skip-gram
+            #Skip-gram
             else:
                 for context_word in context:
                     # Init neu1e with zeros
@@ -289,9 +294,8 @@ def train_process(pid):
                         classifiers = zip(vocab[token].path, vocab[token].code)
                     for target, label in classifiers:
                         z = np.dot(syn0[context_word], syn1[target])
-                        p = sigmoid(z)
-                        g = alpha * (label - p)
-                        neu1e += g * syn1[target]              # Error to backpropagate to syn0
+                        g = alpha * (label - sigmoid(z))
+                        neu1e += g * syn1[target] # Error to backpropagate to syn0
                         syn1[target] += g * syn0[context_word] # Update syn1
 
                     # Update syn0
@@ -307,6 +311,7 @@ def train_process(pid):
     sys.stdout.flush()
     fi.close()
 
+
 def save(vocab, syn0, fo, binary):
     print 'Saving model to', fo
     dim = len(syn0[0])
@@ -319,26 +324,28 @@ def save(vocab, syn0, fo, binary):
             for s in vector:
                 fo.write(struct.pack('f', s))
             fo.write('\n')
+        fo.close()
     else:
         fo = open(fo, 'w')
         fo.write('%d %d\n' % (len(syn0), dim))
         for token, vector in zip(vocab, syn0):
             word = token.word
-            vector_str = ' '.join([str(s) for s in vector])
+            vector_str = ' '.join(['{0:.6f}'.format(s) for s in vector])
             fo.write('%s %s\n' % (word, vector_str))
+        fo.close()
 
-    fo.close()
 
 def __init_process(*args):
     global vocab, syn0, syn1, table, cbow, neg, dim, starting_alpha
     global win, num_processes, global_word_count, fi
-    
+
     vocab, syn0_tmp, syn1_tmp, table, cbow, neg, dim, starting_alpha, win, num_processes, global_word_count = args[:-1]
     fi = open(args[-1], 'r')
     with warnings.catch_warnings():
         warnings.simplefilter('ignore', RuntimeWarning)
         syn0 = np.ctypeslib.as_array(syn0_tmp)
         syn1 = np.ctypeslib.as_array(syn1_tmp)
+
 
 def train(fi, fo, cbow, neg, dim, alpha, win, min_count, num_processes, binary):
     # Read train file to init vocab
@@ -349,18 +356,16 @@ def train(fi, fo, cbow, neg, dim, alpha, win, min_count, num_processes, binary):
 
     global_word_count = Value('i', 0)
     table = None
-    if neg > 0:
+    if neg > 0: # negative sampling
         print 'Initializing unigram table'
         table = UnigramTable(vocab)
-    else:
+    else: # hierarchical softmax
         print 'Initializing Huffman tree'
         vocab.encode_huffman()
 
     # Begin training using num_processes workers
     t0 = time.time()
-    pool = Pool(processes=num_processes, initializer=__init_process,
-                initargs=(vocab, syn0, syn1, table, cbow, neg, dim, alpha,
-                          win, num_processes, global_word_count, fi))
+    pool = Pool(processes=num_processes, initializer=__init_process, initargs=(vocab, syn0, syn1, table, cbow, neg, dim, alpha, win, num_processes, global_word_count, fi))
     pool.map(train_process, range(num_processes))
     t1 = time.time()
     print
@@ -369,20 +374,33 @@ def train(fi, fo, cbow, neg, dim, alpha, win, min_count, num_processes, binary):
     # Save model to file
     save(vocab, syn0, fo, binary)
 
+
+"""
+args to be specified as follows:
+    -train      (fi) training file
+    -model      (fo) Output file to save the model
+    -cbow       (cbow) 1 for CBOW, 0 for skip-gram, default=1
+    -negative   (neg) Number of negative examples (>0) for negative sampling, 0 for hierarchical softmax, default=5
+    -dim        (dim) Dimensionality of word embeddings, default=100
+    -alpha      (alpha) Starting alpha, default=0.025
+    -window     (win) Max window length, default=5
+    -min_count  (min_count) Min count for words used to learn <unk>, default=5
+    -processes  (num_processes) Number of processes, default=1
+    -binary     (binary) 1 for output model in binary format, 0 otherwise, default=0
+"""
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-train', help='Training file', dest='fi', required=True)
-    parser.add_argument('-model', help='Output model file', dest='fo', required=True)
+    parser.add_argument('-train', help='Traing file', dest='fi', required=True)
+    parser.add_argument('-model', help='Output file to save the model', dest='fo', required=True)
     parser.add_argument('-cbow', help='1 for CBOW, 0 for skip-gram', dest='cbow', default=1, type=int)
-    parser.add_argument('-negative', help='Number of negative examples (>0) for negative sampling, 0 for hierarchical softmax', dest='neg', default=5, type=int)
+    parser.add_argument('-negative', help='Number of negative examples(>0) for negative sampling, 0 for hierarchical softmax', dest='neg', default=5, type=int)
     parser.add_argument('-dim', help='Dimensionality of word embeddings', dest='dim', default=100, type=int)
     parser.add_argument('-alpha', help='Starting alpha', dest='alpha', default=0.025, type=float)
-    parser.add_argument('-window', help='Max window length', dest='win', default=5, type=int) 
+    parser.add_argument('-window', help='Max window length', dest='win', default=5, type=int)
     parser.add_argument('-min-count', help='Min count for words used to learn <unk>', dest='min_count', default=5, type=int)
     parser.add_argument('-processes', help='Number of processes', dest='num_processes', default=1, type=int)
     parser.add_argument('-binary', help='1 for output model in binary format, 0 otherwise', dest='binary', default=0, type=int)
     #TO DO: parser.add_argument('-epoch', help='Number of training epochs', dest='epoch', default=1, type=int)
     args = parser.parse_args()
 
-    train(args.fi, args.fo, bool(args.cbow), args.neg, args.dim, args.alpha, args.win,
-          args.min_count, args.num_processes, bool(args.binary))
+    train(args.fi, args.fo, bool(args.cbow), args.neg, args.dim, args.alpha, args.win, args.min_count, args.num_processes, bool(args.binary))
